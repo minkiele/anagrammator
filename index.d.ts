47c4052c7e115de10adeeddd103325ac66b82961
Michele Montagner <michelasso@gmail.com>
2021-11-10 08:31:44 +0100

    Create the stupid-ass anagrammator

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * This function anagrams in a very stupid way a string
+ * This function does not use clever ways to find duplicates,
+ * it just iterates all the possible combination of letters
+ * as if they were al different.
+ * @param input string The string to anagram
+ * @returns An array with all the anagrams for the input string
+ */
+declare function anagrammator(input: string): Array<string>;
+export default anagrammator;
