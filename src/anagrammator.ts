ca46d0e0c34f786529c67b1d729a53592cacee87
Michele Montagner <2039317+minkiele@users.noreply.github.com>
2024-09-04 12:37:44 +0200

    fixed call stack error when generating big anagrams (7+ letters)

--- src/anagrammator.ts
+++ src/anagrammator.ts
@@ -12,14 +12,14 @@ function anagrammator(input: string): Array<string> {
   } else if (input.length === 1) {
     return [input];
   } else {
-    const output: Array<string> = [];
+    let output: Array<string> = [];
     const usedLetters: Array<string> = [];
     for (let i = 0; i < input.length; i += 1) {
       const currentChar = input.charAt(i);
       if (!usedLetters.includes(currentChar)) {
         usedLetters.push(currentChar);
-        output.push(
-          ...anagrammator(`${input.substring(0, i)}${input.substring(i + 1)}`).map(
+        output = output.concat(
+          anagrammator(`${input.substring(0, i)}${input.substring(i + 1)}`).map(
             (anagram) => `${anagram}${currentChar}`,
           ),
         );
